680ec61863873733e8bc1502794a2a117b3ed9b5
Jerome Burkard <jerome.burkard@gadz.org>
2017-02-18 19:36:02 +0100

    crud is fine

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+
+  before_action :find_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    raise
+    @dose.find(params[:id])
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients_array = JSON.parse(url)["drinks"]
+
+ingredients_array.each do |ingredient|
+  ingredient_string = ingredient["strIngredient1"]
+  Ingredient.create!(name: ingredient_string)
+end
+
+Cocktail.create!(name: "Mojito")
+Cocktail.create!(name: "Bloody Marry")
+
+puts "---------------  Seeded with success ^_^ -----------------"
