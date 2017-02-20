aecb8ee928aae42e885e4aeeac8ac0f015574137
Jerome Burkard <jerome.burkard@gadz.org>
2017-02-20 13:23:33 +0100

    starting to implement pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Attachinary::Engine => "/attachinary"
+
   get '/', to: 'cocktails#index'
 
   resources :cocktails, only: [ :index, :show, :new, :create] do
